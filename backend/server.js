2640c7c77eba7a87d37c45db7cf6a4fff6e569eb
mohammadreza tahzibi <m.r.tahzibi@gmail.com>
2022-03-31 10:10:30 -0700

    database setup, add routes and controller, connect to mongoDB, JWT, protect routes and authentication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log("server ...");
+// Import modules
+const express = require("express");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const dotenv = require("dotenv").config({ path: `${__dirname}/.env` });
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+// Connect to database
+connectDB();
+
+// set port
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the support desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Using error handler
+app.use(errorHandler);
+
+// Fire the server
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
